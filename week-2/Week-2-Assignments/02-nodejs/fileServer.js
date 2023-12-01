d9e6f55eb723811041d2246689d38438d2f4695b
tachyonok <tachyonparticle123@gmail.com>
2023-12-01 13:25:24 +0530

    2nd assigment week 2 done

--- week-2/Week-2-Assignments/02-nodejs/fileServer.js
+++ week-2/Week-2-Assignments/02-nodejs/fileServer.js
@@ -16,10 +16,49 @@
 
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
+// The expected API endpoints are defined below,
+// 1. GET /files - Returns a list of files present in `./files/` directory
+//   Response: 200 OK with an array of file names in JSON format.
+//   Example: GET http://localhost:3000/files
+
+app.get("/files", (req, res) => {
+  // how to return the files in a directory from a path
+  fs.readdir(path.join(__dirname, "./files/"), (err, files) => {
+    if (err) {
+      res.status(500).json({ error: "file not found" });
+    } else {
+      res.json(files);
+    }
+  });
+});
+
+// 2. GET /file/:filename - Returns content of given file by name
+//      Description: Use the filename from the request path parameter to read the file from `./files/` directory
+//      Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+//      Example: GET http://localhost:3000/file/example.txt
+
+//     - For any other route not defined in the server return 404
+
+app.get("/file/:filename", (req, res) => {
+  //adding the filepath as specified from the queryParams
+  let filePath = fs.readdir(__dirname, "./files/", req.params.filename);
+
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      res.status(404).send("File not found");
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Route not found");
+});
 
 module.exports = app;
